7263d050ad0c925b7387333c7fd7d233b41f362a
guylamar2006 <guychronister@outlook.com>
2025-02-11 14:44:25 -0600

    Fix ambiguous closure.

--- src/platforms/playnite/playnite_parser.rs
+++ src/platforms/playnite/playnite_parser.rs
@@ -33,7 +33,7 @@ fn parse_game(i: &[u8]) -> nom::IResult<&[u8], NamesAndId> {
     let (i, _taken) = take_until("InstallSizeGroup")(i)?;
     let (i, _taken) = take_until("Name")(i)?;
     let (i, _taken) = take(4usize)(i)?;
-    let (i, _taken) = take_while(|b| !b.is_alphanum())(i)?;
+    let (i, _taken) = take_while(|b: u8| !b.is_alphanum())(i)?;
     let (i, name_bytes) = take_while(|b| b != 0)(i)?;
     let name = String::from_utf8_lossy(name_bytes).to_string();
     IResult::Ok((
